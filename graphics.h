3afd6e2b99627e892badb8bc837e857d046232cb
Bosha1uk <barisballi@hotmail.com>
2016-02-08 20:27:34 +0000

    Create graphics.h

---
+++ graphics.h
@@ -0,0 +1,8 @@
+#ifndef _GRAPHICS
+#define _GRAPHICS
+
+void Graphics_Init(int w, int h);
+void Graphics_ClearScreen();
+void Graphics_DrawQuad(float x, float y, float w, float h, float r, float g, float b, float a);
+
+#endif
